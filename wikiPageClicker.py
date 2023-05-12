e9959d8c03ab445c5269560f2d49e37d8280534c
AustenVanBrogen <austenvanbrogen@yahoo.com>
2023-05-12 18:37:54 -0500

    Program now retrieves all the links from the body of a wiki page. Next issues will be filtering out the non-wiki links and then clicking on the links

--- wikiPageClicker.py
+++ wikiPageClicker.py
@@ -5,9 +5,17 @@ wikiUrl = "https://en.wikipedia.org/wiki/Garfield"
 pageToScrape = requests.get(wikiUrl)
 soup = BeautifulSoup(pageToScrape.text, 'html.parser')
 
-with open(r"deleteMe.txt", "w", encoding='utf-8') as oWrite:
-    oWrite.write(str(soup.prettify))
-    oWrite.close()
+# Saves the html to a file to read it. DELETE LATER 
+# with open(r"deleteMe.txt", "w", encoding='utf-8') as oWrite:
+#     oWrite.write(str(soup.prettify))
+#     oWrite.close()
 
+#print(soup.title)
 
-#print(soup.prettify)
+body = soup.find_all("div", {"class": "vector-body"})
+#print(body[0])
+aTags = body[0].find_all("a") #Body is a list so we need the 0th index
+aTags.pop(0) #The fist element is about protection policy, so we get rid of it
+
+for link in aTags:
+    print(link.get("href"))
