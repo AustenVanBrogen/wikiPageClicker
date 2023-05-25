165170a5ad1ed96a424a887783e57c1a859db677
AustenVanBrogen <austenvanbrogen@yahoo.com>
2023-05-25 18:40:07 -0500

    Made the program a function so it can be called for each wiki page needed

--- wikiPageClicker.py
+++ wikiPageClicker.py
@@ -2,34 +2,33 @@ from bs4 import BeautifulSoup
 import requests
 
 wikiUrl = "https://en.wikipedia.org/wiki/Garfield"
-pageToScrape = requests.get(wikiUrl)
-soup = BeautifulSoup(pageToScrape.text, 'html.parser')
-
 substr = ['/wiki/']
-links = []
-filteredLinks = []
 
-#Saves the html to a file to read it. DELETE LATER 
-# with open(r"deleteMe.txt", "w", encoding='utf-8') as oWrite:
-#     oWrite.write(str(soup.prettify()))
-#     oWrite.close()
+def checkPage(curURL):
+    links = []
+    filteredLinks = []
 
-body = soup.find_all("div", {"class": "vector-body"})
-aTags = body[0].find_all("a") #Body is a list so we need the 0th index
-aTags.pop(0) #The fist element is about protection policy, so we get rid of it
+    pageToScrape = requests.get(curURL)
+    soup = BeautifulSoup(pageToScrape.text, 'html.parser')
+    body = soup.find_all("div", {"class": "vector-body"})
+    aTags = body[0].find_all("a") #Body is a list so we need the 0th index
+    aTags.pop(0) #The fist element is about protection policy, so we get rid of it
 
-for link in aTags:
-    links.append(link.get("href"))
+    for link in aTags:
+        links.append(link.get("href"))
 
-#Some of the instances in link were NoneType, so those are ignored
-for i in substr:
-    for j in links:
-        if(isinstance(j, str) and j.find(i)==0 and j not in filteredLinks):
-            filteredLinks.append(j)
+    #Some of the instances in link were NoneType, so those are ignored
+    for i in substr:
+        for j in links:
+            if(isinstance(j, str) and j.find(i)==0 and j not in filteredLinks):
+                filteredLinks.append(j)
 
-#Clears aTags and links to save resources
-links.clear()
-aTags.clear()
+    #Clears aTags and links to save resources
+    links.clear()
+    aTags.clear()
 
-for i in filteredLinks:
-    print(i)
+    #For testing. Delete Later
+    for i in filteredLinks:
+        print(i)
+
+checkPage("https://en.wikipedia.org/wiki/Garfield")
