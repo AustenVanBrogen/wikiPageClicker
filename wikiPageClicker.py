133835810bcabf040933d98b5ad2fef4f0258bda
AustenVanBrogen <austenvanbrogen@yahoo.com>
2023-05-29 15:16:27 -0500

    Added some more filters for the links to get rid of unwanted pages and file types. Also added a depth input for the checkPage function

--- wikiPageClicker.py
+++ wikiPageClicker.py
@@ -4,31 +4,61 @@ import requests
 wikiUrl = "https://en.wikipedia.org/wiki/Garfield"
 substr = ['/wiki/']
 
-def checkPage(curURL):
-    links = []
-    filteredLinks = []
+def fitsCriteria(link):
+    if(link.find('/wiki/Category:') != -1):
+        return False
+    if(link.find('/wiki/Help:') != -1):
+        return False
+    if(link.find('/wiki/Portal:') != -1):
+        return False
+    if(link.find('/wiki/File:') != -1):
+        return False
+    if(link.find('/wiki/Wikipedia:') != -1):
+        return False
+    if(link.find('/wiki/Special:') != -1):
+        return False
+    if(link.find('/wiki/Template:') != -1):
+        return False
+    if(link.find('png') != -1):
+        return False
+    if(link.find('jpg') != -1):
+        return False
+    if(link.find('jpeg') != -1):
+        return False
+    if(link.find('svg') != -1):
+        return False
+    if(link.find('webp') != -1):
+        return False
+    return True
 
-    pageToScrape = requests.get(curURL)
-    soup = BeautifulSoup(pageToScrape.text, 'html.parser')
-    body = soup.find_all("div", {"class": "vector-body"})
-    aTags = body[0].find_all("a") #Body is a list so we need the 0th index
-    aTags.pop(0) #The fist element is about protection policy, so we get rid of it
+def checkPage(curURL, depth):
+    try:
+        links = []
+        filteredLinks = []
 
-    for link in aTags:
-        links.append(link.get("href"))
+        pageToScrape = requests.get("https://en.wikipedia.org{}".format(curURL))
+        soup = BeautifulSoup(pageToScrape.text, 'html.parser')
+        body = soup.find_all("div", {"class": "vector-body"})
+        aTags = body[0].find_all("a") #Body is a list so we need the 0th index
+        aTags.pop(0) #The fist element is about protection policy, so we get rid of it
 
-    #Some of the instances in link were NoneType, so those are ignored
-    for i in substr:
-        for j in links:
-            if(isinstance(j, str) and j.find(i)==0 and j not in filteredLinks):
-                filteredLinks.append(j)
+        for link in aTags:
+            links.append(link.get("href"))
 
-    #Clears aTags and links to save resources
-    links.clear()
-    aTags.clear()
+        #Some of the instances in link were NoneType, so those are ignored
+        for i in substr:
+            for j in links:
+                if(isinstance(j, str) and j.find(i)==0 and fitsCriteria(j) and j not in filteredLinks):
+                    filteredLinks.append(j)
 
-    #For testing. Delete Later
-    for i in filteredLinks:
-        print(i)
+        #Clears aTags and links to save resources
+        links.clear()
+        aTags.clear()
 
-checkPage("https://en.wikipedia.org/wiki/Garfield")
+        #For testing. Delete Later
+        for i in filteredLinks:
+            print(i)
+    except:
+        return ''
+
+checkPage("/wiki/Garfield", 0)
